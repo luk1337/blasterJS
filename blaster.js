9e32a31a838fd6770ed94306a3d3862ef03b1279
LuK1337 <priv.luk@gmail.com>
2016-01-13 20:17:13 +0100

    Get rid of single letter variables and fix code style once again

--- blaster.js
+++ blaster.js
@@ -1,8 +1,7 @@
 var Blaster = (function Blaster() {
     "use strict";
-    var _privateVars = {
-    };
 
+    var _privateVars = { };
     var _defaultOptions = {
         resizeVar: "window.innerHeight",
         definedParam: 1600,
@@ -15,38 +14,38 @@ var Blaster = (function Blaster() {
     return function BlasterConstructor(arg) {
         var _this = this; // Cache the `this` keyword
 
-        _this.merge = function(a, b) {
-            for (var p in b) {
+        _this.mergeOptions = function(oldOptions, newOptions) {
+            for (var option in newOptions) {
                 try {
-                    if (b[p].constructor == Object) {
-                        a[p] = _this.mergeRecursive(a[p], b[p]);
+                    if (newOptions[p].constructor == Object) {
+                        oldOptions[option] = _this.mergeRecursive(oldOptions[option], newOptions[option]);
                     } else {
-                        a[p] = b[p];
+                        oldOptions[option] = newOptions[option];
                     }
-                } catch(e) {
-                    a[p] = b[p];
+                } catch(exception) {
+                    oldOptions[option] = newOptions[option];
                 }
             }
 
-            return a;
+            return oldOptions;
         };
 
         /**
-         *  Handlers
-         *
+         * Handlers
          */
 
         _this.generateHandler = function () {
             var generateOutput = _this.generate();
-            //_privateVars.styleElement.innerHTML = "";
             _privateVars.styleElement.innerHTML = generateOutput.cssOutput;
             _this.vars = generateOutput.varsOutput;
+
             generateOutput = null;
         };
 
         _this.resizeHandler = function () {
             _this.options.resizeParam = eval(_this.options.resizeVar);
             _privateVars.globalRatio = (_this.options.resizeParam / _this.options.definedParam);
+
             _this.generateHandler();
         };
 
@@ -63,27 +62,25 @@ var Blaster = (function Blaster() {
                 if (_this.resources.hasOwnProperty(resourceName)) {
                     var resource = _this.resources[resourceName];
 
-                    // property nie zostalo dziedziczone więc luz
                     cssOutput += resourceName + " { ";
 
-                    for (var r in resource) {
-                        var currentResource = resource[r];
-                        cssOutput += r + ": ";
+                    for (var resourceId in resource) {
+                        var currentResource = resource[resourceId];
+                        cssOutput += resourceId + ": ";
 
                         if (typeof currentResource[1] === 'string') {
-                            cssOutput += resource[r] + "; ";
+                            cssOutput += resource[resourceId] + "; ";
                         } else if (currentResource[1] != undefined &&
-                                currentResource[1] != false &&
-                                typeof currentResource[1] !== 'object') {
+                            currentResource[1] != false &&
+                            typeof currentResource[1] !== 'object') {
                             cssOutput += (currentResource[0] *
-                                    (currentResource[1] * _privateVars.globalRatio)).toFixed(1);
+                                (currentResource[1] * _privateVars.globalRatio)).toFixed(1);
 
                             if (currentResource[2] != undefined) {
                                 cssOutput += currentResource[2];
                             }
 
                             cssOutput += "; ";
-
                         } else if (typeof currentResource[1] === 'object') {
                             paramArray = [];
 
@@ -91,15 +88,20 @@ var Blaster = (function Blaster() {
                                 if (_this.options.resizeParam <= param) {
                                     if (paramArray[0] != null) {
                                         if (paramArray[0] >= (_this.options.resizeParam - param)) {
-                                            paramArray[param] =[_this.options.resizeParam - param, currentResource[1][param]];
+                                            paramArray[param] =
+                                                [_this.options.resizeParam - param, currentResource[1][param]];
                                         }
                                     } else {
-                                        paramArray[param] = [_this.options.resizeParam - param, currentResource[1][param]];
+                                        paramArray[param] =
+                                            [_this.options.resizeParam - param, currentResource[1][param]];
                                     }
                                 }
 
-                                if (currentResource[1][param][1] != undefined && typeof currentResource[1][param][0] === "number") {
-                                    cssOutput += (currentResource[1][param][0] * (currentResource[1][param][1] * _privateVars.globalRatio)).toFixed(1) + currentResource[2] + "; ";
+                                if (currentResource[1][param][1] != undefined &&
+                                    typeof currentResource[1][param][0] === "number") {
+                                    cssOutput += (currentResource[1][param][0] *
+                                        (currentResource[1][param][1] * _privateVars.globalRatio)).toFixed(1) +
+                                        currentResource[2] + "; ";
                                 } else {
                                     cssOutput += currentResource[1][param][0] + "; ";
                                 }
@@ -111,11 +113,12 @@ var Blaster = (function Blaster() {
 
                     cssOutput += "} ";
 
-                    for (var v in resource.vars) {
-                        if (resource.vars[r][1] != undefined && resource.vars[r][1] != false) {
-                            varsOutput[r][v] = (resource.vars[r][0] * (resource.vars[r][1] * _privateVars.globalRatio)).toFixed(1);
+                    for (var variable in resource.vars) {
+                        if (resource.vars[resourceId][1] != undefined && resource.vars[resourceId][1] != false) {
+                            varsOutput[resourceId][variable] = (resource.vars[resourceId][0] *
+                                (resource.vars[resourceId][1] * _privateVars.globalRatio)).toFixed(1);
                         } else {
-                            varsOutput[r][v] = resource.vars[r][0];
+                            varsOutput[resourceId][variable] = resource.vars[resourceId][0];
                         }
                     }
 
@@ -128,13 +131,14 @@ var Blaster = (function Blaster() {
             };
         };
 
-        _this.options = _this.merge(_defaultOptions, arg.options);
+        _this.options = _this.mergeOptions(_defaultOptions, arg.options);
         _this.resources = arg.resources;
         _this.vars = {};
 
         if (_this.options.autoStyleElem) {
             _privateVars.styleElement = document.createElement("style");
             _privateVars.styleElement.setAttribute("id", _this.options.styleElemId);
+
             document.body.appendChild(_privateVars.styleElement);
         }
 
@@ -148,7 +152,7 @@ var Blaster = (function Blaster() {
             } else if(window.addEventListener) {
                 window.addEventListener('resize', _this.resizeHandler, true);
             } else {
-                console.warn("Blaster","This browser doesn't support resizing event binding")
+                console.warn("Blaster", "This browser doesn't support resizing event binding");
             }
         }
     };
